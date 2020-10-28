169b40d6517e515e623d56821ae7c4acbb2f3384
Gumparthy Pavan Kumar <gumparthypk@gmail.com>
2020-10-28 11:26:52 +0530

    Added Redux and implement alert feauture

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,32 @@ import "./App.css";
 //Components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Alert from "./components/layout/Alert";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+//Provider from react-redux
+import { Provider } from "react-redux";
+
+//Redux Store
+import store from "./store";
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={ Landing }></Route>
-        <section className="container">
-          <Switch>
-            <Route exact path="/register" component={ Register }></Route>
-            <Route exact path="/login" component={ Login }></Route>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={ store }>
+      <Router>
+        <Fragment>
+          <Navbar/>
+          <Route exact path="/" component={Landing}></Route>
+          <section className="container">
+            <Alert/>
+            <Switch>
+              <Route exact path="/register" component={Register}></Route>
+              <Route exact path="/login" component={Login}></Route>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,19 +1,39 @@
 import React, { Fragment,useState} from "react";
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { PropTypes } from 'prop-types';
 
-const Register = () => {
-const [formData,setFormData] = useState;
+const Register = ({setAlert}) => {
+  const [formData,setFormData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    password2:''
+  });
+
+  const { name,email,password,password2 } = formData;
+  const onChange = (e) => setFormData({...formData,[e.target.name] : e.target.value})
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(password !== password2){
+      setAlert('Passwords Do Not Match','danger');
+    }
+    else{
+      console.log('SUCCESS');
+    }
+  }
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" style={InputStyling}required />
+          <input type="text" placeholder="Name" name="name" style={InputStyling} value ={name} onChange={e => onChange(e)} required/>
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" style={InputStyling}name="email" />
+          <input type="email" placeholder="Email Address" style={InputStyling} name="email" value={email} onChange={e => onChange(e)} required/>
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -24,6 +44,8 @@ const [formData,setFormData] = useState;
             type="password"
             placeholder="Password"
             name="password" 
+            value={password}
+            onChange={e => onChange(e)}
             style={InputStyling}
             minLength="6"
           />
@@ -33,6 +55,8 @@ const [formData,setFormData] = useState;
             type="password"
             placeholder="Confirm Password"
             name="password2" 
+            value={password2}
+            onChange={e => onChange(e)}
             style={InputStyling}
             minLength="6"
           />
@@ -49,4 +73,9 @@ const [formData,setFormData] = useState;
 const InputStyling = {
     borderRadius:'10px'
 }
-export default Register;
+
+Register.propTypes = {
+  setAlert : PropTypes.func.isRequired
+}
+
+export default connect(null,{setAlert})(Register);
